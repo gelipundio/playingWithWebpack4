d2d8d0b8bf419bcb554aa9924cf8bcd90bdbd028
Felipe Nevarez <wolf.antica@gmail.com>
2019-07-12 17:22:29 -0500

    Playing with react jsx

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
 
 function App() {
+  const [loaderList, setLoaderList] = useState([])
+
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
+
   return (
     <div>
+      <ul>
+        {
+          loaderList.map(({name, id}) => <Loader name={name} key={id} />)
+        }
+      </ul>
       que linda app hecha en React.js
+
+      <button onClick={handleClick}>Mostrar lo aprendido</button>
     </div>
   )
 }
